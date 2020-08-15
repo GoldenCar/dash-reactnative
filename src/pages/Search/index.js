750a445863b3b8ed3de3685dabb4615bb5b85e59
Shane Duggan <devshaneduggan@gmail.com>
2020-08-15 14:12:01 -0700

    Cleans up Search page

--- src/pages/Search/index.js
+++ src/pages/Search/index.js
@@ -1,92 +1,35 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import {
   View,
   StyleSheet,
-  TouchableWithoutFeedback,
   ScrollView,
   Text,
   TouchableOpacity
 } from 'react-native';
 import {connect} from 'react-redux';
-
 import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
-
-import Challenge from '../../components/Challenge';
+import {Actions} from 'react-native-router-flux';
 
 import Search from '../../components/Search';
-import {Close, BackArrow} from '../../components/Icons';
-
-import NavBar from '../../components/NavBar';
-import Plan from '../../components/Plan';
-
-import {Actions} from 'react-native-router-flux';
-import * as plansActions from '../../actions/plans';
 
 function Component(props) {
- 
-  const challenges = props.challenges.filter(
-    (v) => v.Featured === "yes",
-  );
-
-  const [plans, setPlans] = useState([]);
-  useEffect(() => {
-    const init = async () => {
-      try {
-        const data = await plansActions.getPlans();
-        console.log("categories1.....", data)
-        setPlans(data);
-      } catch (e) {}
-    };
-    init();
-  }, []);
-
   return (
     <View style={styles.container}>
-      <ScrollView contentContainerStyle={styles.contentContainerStyle} bounces={false}>
+      <View style={{
+        paddingBottom: 20
+      }}>
         <Text style={styles.title}>Search</Text>
-          <Search />
+        <Search autoFocus />
 
-          {/* <Close /> */}
-          <TouchableOpacity onPress={() => Actions.pop()} style={{width: 40, alignItems:"center"}}>
-            {/* <BackArrow /> */}
-            <Icon name={'close'} color="#B6BCCA" size={20} />
-          </TouchableOpacity>
+        <TouchableOpacity 
+          onPress={() => Actions.pop()}
+          style={styles.close}
+        >
+          <Icon name='close' color="#B6BCCA" size={20} />
+        </TouchableOpacity>
+      </View>
 
-        {/* <View style={styles.challengesContainer}>
-          <ScrollView
-            horizontal
-            pagingEnabled
-            showsHorizontalScrollIndicator={false}
-          >
-            {challenges.map((value, index) => (
-              <TouchableWithoutFeedback
-                key={index}
-                onPress={() => Actions.ChallengeDetail({challenge: value})}
-                // onPress={() => Actions.ExplorePost()}
-                >
-                <View>
-                  <Challenge value={value} viewedBy={viewedBy} explore />
-                </View>
-              </TouchableWithoutFeedback>
-            ))}
-          </ScrollView>
-
-          <View style={styles.hostContainer}>
-            <Text style={[styles.title, { marginBottom: 10 }]}>Host a Challenge</Text>
-            <Text style={styles.subtitle}>
-              Choose one of our plans and host a challenge for you and your friends
-            </Text>
-          </View>
-          {plans.map((value, index) => (
-              <TouchableWithoutFeedback
-                key={index}
-                onPress={() => {}}
-                >
-                  <Plan value={value} />
-              </TouchableWithoutFeedback>
-            ))}
-        </View> */}
-      </ScrollView>
+      <ScrollView contentContainerStyle={styles.contentContainerStyle} bounces={false}></ScrollView>
     </View>
   );
 }
@@ -101,26 +44,26 @@ const styles = StyleSheet.create({
     color: '#21293D',
     marginHorizontal: 15,
   },
-  subtitle: {
-    fontFamily: 'Poppins-Regular',
-    fontSize: 14,
-    color: '#21293D',
-    marginHorizontal: 15
-  },
   container: {
     flex: 1,
+    paddingTop: 30
   },
   contentContainerStyle: {
     paddingTop: 30,
     paddingBottom: 100,
+    backgroundColor: '#B6BCCA',
+    flex: 1
   },
-  search: {
+  close: {
     position: 'absolute',
     right: 20,
-    top: 30
-  },
-  hostContainer: {
-    marginBottom: 30
+    height: 40,
+    width: 40,
+    borderRadius: 20,
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderWidth: 0.5,
+    borderColor: '#96AAC6'
   }
 });
 export default connect(({challenges}) => ({
