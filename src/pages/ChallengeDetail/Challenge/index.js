a43d373aacce7e639b9406784dd70c35837070b3
Shane Duggan <devshaneduggan@gmail.com>
2020-08-27 14:28:26 -0700

    Adds Social section to ChallengeDetail, cleans up Social & Post component design

--- src/pages/ChallengeDetail/Challenge/index.js
+++ src/pages/ChallengeDetail/Challenge/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
-import { View, ScrollView, Dimensions, Text, Animated, Image } from 'react-native';
+import { View, ScrollView, Dimensions, Text, Animated, Image, TouchableOpacity } from 'react-native';
 import EStyleSheet from 'react-native-extended-stylesheet';
+import { Actions } from 'react-native-router-flux';
 
 import Header from './Header';
 import Countdown from '../../../components/Countdown';
@@ -18,13 +19,24 @@ export default class Component extends React.Component {
   PopupPostRef;
   ScrollViewAnimation = new Animated.Value(0);
 
+  state = {
+    contentHeight: 1000
+  }
+
   render() {
+    const { contentHeight } = this.state;
     const { challenge, plan, user } = this.props;
 
     const onScroll = Animated.event(
       [{ nativeEvent: { contentOffset: { y: this.ScrollViewAnimation } } }],
       { useNativeDriver: false },
     );
+
+    const opacity = this.ScrollViewAnimation.interpolate({
+      inputRange: [0, contentHeight - 1, contentHeight],
+      outputRange: [0, 0, 1],
+      extrapolate: 'clamp',
+    });
 
     return (
       <View style={styles.container}>
@@ -39,32 +51,40 @@ export default class Component extends React.Component {
           <View style={styles.innerContainer}>
             <Text style={styles.blueText}>Hosted by {challenge.Host}</Text>
             <Text style={styles.title}>{challenge.title}</Text>
-            <Countdown
-              initialTime={60}
-              centerTitle
-              containerStyle={styles.countdownContainer}
-            />
 
-            <View style={styles.inviteFriends}>
-              <Image
-                source={FriendImage}
+            <View
+              style={styles.contentContainer}
+              onLayout={(event) => {
+                const { x, y, width, height } = event.nativeEvent.layout;
+                console.log('content container height', height);
+                this.setState({ contentHeight: height });
+              }}
+            >
+              <Countdown
+                initialTime={60}
+                centerTitle
+                containerStyle={styles.countdownContainer}
               />
-              <View style={styles.friendTextContainer}>
-                <Text style={styles.friendTitle}>Invite Friends</Text>
-                <Text style={styles.friendSubtitle}>
-                  Invite your friends to this challenge so they can join in on the fun.
-                </Text>
+              <View style={styles.inviteFriends}>
+                <Image
+                  source={FriendImage}
+                />
+                <View style={styles.friendTextContainer}>
+                  <Text style={styles.friendTitle}>Invite Friends</Text>
+                  <Text style={styles.friendSubtitle}>
+                    Invite your friends to this challenge so they can join in on the fun.
+                  </Text>
+                </View>
+                <View style={styles.friendButton}>
+                  <Plus fill='#fff' />
+                  <Text style={styles.friendButtonText}>
+                    Invite Friends
+                  </Text>
+                </View>
               </View>
-              <View style={styles.friendButton}>
-                <Plus fill='#fff' />
-                <Text style={styles.friendButtonText}>
-                  Invite Friends
-                </Text>
+              <View style={styles.planContainer}>
+                <Plan value={plan} blueButton />
               </View>
-            </View>
-
-            <View style={styles.planContainer}>
-              <Plan value={plan} blueButton />
             </View>
 
             <View style={{ width, height }}>
@@ -77,6 +97,13 @@ export default class Component extends React.Component {
           </View>
         </ScrollView>
         <PopupPost ref={(e) => (this.PopupPostRef = e)} />
+
+        <TouchableOpacity onPress={() => Actions.CreatePost({ challenge })}>
+          <Animated.View style={[styles.addPostButton, { opacity }]}>
+            <Plus fill='#fff' />
+            <Text style={styles.addPostText}>New Post</Text>
+          </Animated.View>
+        </TouchableOpacity>
       </View>
     );
   }
@@ -92,9 +119,11 @@ const styles = EStyleSheet.create({
     backgroundColor: 'white',
     flex: 1,
     marginTop: height / 2 - 20,
-    paddingHorizontal: 16,
     paddingTop: 34,
-    marginBottom: 208
+    paddingBottom: 208
+  },
+  contentContainer: {
+    paddingHorizontal: 16,
   },
   countdownContainer: {
     paddingHorizontal: 56,
@@ -162,8 +191,27 @@ const styles = EStyleSheet.create({
     textAlign: 'center',
     color: '#FFFFFF',
   },
-
   planContainer: {
-    marginTop: 13
+    marginTop: 13,
+    marginBottom: 16
+  },
+  addPostButton: {
+    width: 138,
+    height: 56,
+    borderRadius: 28,
+    position: 'absolute',
+    bottom: 36,
+    right: 18,
+    backgroundColor: '#1AA0FF',
+    //box-shadow: 0px 20px 20px rgba(26, 160, 255, 0.25);
+    flexDirection: 'row',
+    paddingHorizontal: 22,
+    justifyContent: 'space-between',
+    alignItems: 'center'
+  },
+  addPostText: {
+    fontFamily: 'Poppins-Medium',
+    fontSize: 16,
+    color: '#FFFFFF'
   }
 });
