87870417aa96052a50a136ad95d6938b7997c4a9
Shane Duggan <devshaneduggan@gmail.com>
2020-08-28 07:13:14 -0700

    Fixes animations, enables useNativeDriver, moves components to ChallengeDetail page

--- src/pages/ChallengeDetail/Challenge/index.js
+++ src/pages/ChallengeDetail/Challenge/index.js
@@ -1,9 +1,7 @@
 import React from 'react';
-import { View, ScrollView, Dimensions, Text, Animated, Image, TouchableOpacity } from 'react-native';
+import { View, Dimensions, Text, Image } from 'react-native';
 import EStyleSheet from 'react-native-extended-stylesheet';
-import { Actions } from 'react-native-router-flux';
 
-import Header from './Header';
 import Countdown from '../../../components/Countdown';
 import Plan from '../../../components/Plan';
 import PopupPost from '../Social/PopupPost';
@@ -12,99 +10,63 @@ import Social from '../Social'
 import Plus from '../../MyChallenges/Icons/Plus';
 import FriendImage from './invite.png';
 
-const { height, width } = Dimensions.get('window');
-
-const AnimatedTouchable = Animated.createAnimatedComponent(TouchableOpacity);
+const { height } = Dimensions.get('window');
 
 export default class Component extends React.Component {
   AuthPopupRef;
   PopupPostRef;
-  ScrollViewAnimation = new Animated.Value(0);
-
-  state = {
-    contentHeight: 1000
-  }
 
   render() {
-    const { contentHeight } = this.state;
-    const { challenge, plan, user } = this.props;
-
-    const onScroll = Animated.event(
-      [{ nativeEvent: { contentOffset: { y: this.ScrollViewAnimation } } }],
-      { useNativeDriver: false },
-    );
-
-    const opacity = this.ScrollViewAnimation.interpolate({
-      inputRange: [0, contentHeight - 1, contentHeight],
-      outputRange: [0, 0, 1],
-      extrapolate: 'clamp',
-    });
+    const { challenge, plan, user, setContentContainerHeight } = this.props;
 
     return (
       <View style={styles.container}>
-        <Header
-          ScrollViewAnimation={this.ScrollViewAnimation}
-          {...this.props}
-        />
-        <ScrollView
-          onScroll={onScroll}
-          showsVerticalScrollIndicator={false}
-        >
-          <View style={styles.innerContainer}>
-            <Text style={styles.blueText}>Hosted by {challenge.Host}</Text>
-            <Text style={styles.title}>{challenge.title}</Text>
+        <View style={styles.innerContainer}>
+          <Text style={styles.blueText}>Hosted by {challenge.Host}</Text>
+          <Text style={styles.title}>{challenge.title}</Text>
 
-            <View
-              style={styles.contentContainer}
-              onLayout={(event) => {
-                const { height } = event.nativeEvent.layout;
-                this.setState({ contentHeight: height });
-              }}
-            >
-              <Countdown
-                initialTime={60}
-                centerTitle
-                containerStyle={styles.countdownContainer}
+          <View
+            style={styles.contentContainer}
+            onLayout={(event) => {
+              const { height } = event.nativeEvent.layout;
+              setContentContainerHeight(height);
+            }}
+          >
+            <Countdown
+              initialTime={60}
+              centerTitle
+              containerStyle={styles.countdownContainer}
+            />
+            <View style={styles.inviteFriends}>
+              <Image
+                source={FriendImage}
+                style={styles.friendImage}
               />
-              <View style={styles.inviteFriends}>
-                <Image
-                  source={FriendImage}
-                  style={styles.friendImage}
-                />
-                <View style={styles.friendTextContainer}>
-                  <Text style={styles.friendTitle}>Invite Friends</Text>
-                  <Text style={styles.friendSubtitle}>
-                    Invite your friends to this challenge so they can join in on the fun.
+              <View style={styles.friendTextContainer}>
+                <Text style={styles.friendTitle}>Invite Friends</Text>
+                <Text style={styles.friendSubtitle}>
+                  Invite your friends to this challenge so they can join in on the fun.
                   </Text>
-                </View>
-                <View style={styles.friendButton}>
-                  <Plus fill='#fff' />
-                  <Text style={styles.friendButtonText}>
-                    Invite Friends
-                  </Text>
-                </View>
               </View>
-              <View style={styles.planContainer}>
-                <Plan value={plan} blueButton />
+              <View style={styles.friendButton}>
+                <Plus fill='#fff' />
+                <Text style={styles.friendButtonText}>
+                  Invite Friends
+                  </Text>
               </View>
             </View>
-
-            <Social
-              user={user}
-              challenge={challenge}
-              PopupPostRef={this.PopupPostRef}
-            />
+            <View style={styles.planContainer}>
+              <Plan value={plan} blueButton />
+            </View>
           </View>
-        </ScrollView>
-        <PopupPost ref={(e) => (this.PopupPostRef = e)} />
 
-        <AnimatedTouchable
-          onPress={() => Actions.CreatePost({ challenge })}
-          style={[styles.addPostButton, { opacity }]}
-        >
-          <Plus fill='#fff' />
-          <Text style={styles.addPostText}>New Post</Text>
-        </AnimatedTouchable>
+          <Social
+            user={user}
+            challenge={challenge}
+            PopupPostRef={this.PopupPostRef}
+          />
+        </View>
+        <PopupPost ref={(e) => (this.PopupPostRef = e)} />
       </View>
     );
   }
@@ -112,7 +74,7 @@ export default class Component extends React.Component {
 
 const styles = EStyleSheet.create({
   container: {
-    flex: 1,
+    flex: 1
   },
   innerContainer: {
     borderTopLeftRadius: 20,
@@ -121,7 +83,7 @@ const styles = EStyleSheet.create({
     flex: 1,
     marginTop: height / 2 - 20,
     paddingTop: 34,
-    paddingBottom: 208
+    paddingBottom: 208,
   },
   contentContainer: {
     paddingHorizontal: 16,
@@ -198,24 +160,5 @@ const styles = EStyleSheet.create({
   planContainer: {
     marginTop: 13,
     marginBottom: 16
-  },
-  addPostButton: {
-    width: 138,
-    height: 56,
-    borderRadius: 28,
-    position: 'absolute',
-    bottom: 36,
-    right: 18,
-    backgroundColor: '#1AA0FF',
-    //box-shadow: 0px 20px 20px rgba(26, 160, 255, 0.25);
-    flexDirection: 'row',
-    paddingHorizontal: 22,
-    justifyContent: 'space-between',
-    alignItems: 'center'
-  },
-  addPostText: {
-    fontFamily: 'Poppins-Medium',
-    fontSize: 16,
-    color: '#FFFFFF'
   }
 });
--- src/pages/ChallengeDetail/index.js
+++ src/pages/ChallengeDetail/index.js
@@ -1,15 +1,19 @@
 import React from 'react';
-import { View, Dimensions, TouchableOpacity } from 'react-native';
+import { View, Dimensions, TouchableOpacity, Animated, Text } from 'react-native';
 import { connect } from 'react-redux';
 import EStyleSheet from 'react-native-extended-stylesheet';
 import { Actions } from 'react-native-router-flux';
 
-import { Close, Settings } from 'dash/src/components/Icons';
 import Challenge from './Challenge';
+import Header from './Challenge/Header';
+import { Close, Settings } from 'dash/src/components/Icons';
+import Plus from '../MyChallenges/Icons/Plus';
 
 import * as plansActions from '../../actions/plans';
 
 const { width, height } = Dimensions.get('screen');
+
+const AnimatedTouchable = Animated.createAnimatedComponent(TouchableOpacity);
 
 class Component extends React.Component {
     constructor(props) {
@@ -18,9 +22,12 @@ class Component extends React.Component {
 
         this.state = {
             user: challenge.createdBy === user._id ? user : {},
-            plan: {}
+            plan: {},
+            contentHeight: 1000
         };
     }
+
+    ScrollViewAnimation = new Animated.Value(0);
 
     // TODO: need way to get plan with id from server
     async componentDidMount() {
@@ -35,7 +42,7 @@ class Component extends React.Component {
     }
 
     render() {
-        const { plan, user } = this.state;
+        const { plan, user, contentHeight } = this.state;
         const { challenge } = this.props;
         console.log(" challenge is ", challenge);
 
@@ -44,26 +51,62 @@ class Component extends React.Component {
         //          2. Only show countdown if not started
         //          3. Figure out daily progression
 
+        const onScroll = Animated.event(
+            [{ nativeEvent: { contentOffset: { y: this.ScrollViewAnimation } } }],
+            { useNativeDriver: true }
+        );
+
+        const setContentContainerHeight = (height) => this.setState({ contentHeight: height });
+
+        const addPostButtonOpacity = this.ScrollViewAnimation.interpolate({
+            inputRange: [0, contentHeight - 1, contentHeight],
+            outputRange: [0, 0, 1],
+            extrapolate: 'clamp',
+        });
+
+        const buttonOpacity = this.ScrollViewAnimation.interpolate({
+            inputRange: [0, height / 2],
+            outputRange: [1, 0],
+            extrapolate: 'clamp',
+        });
+
         return (
-            <View>
-                <TouchableOpacity
-                    style={[styles.button, styles.backButton]}
-                    onPress={() => Actions.MyChallengesTab()}
+            <View style={{ width, height }}>
+                <Header
+                    ScrollViewAnimation={this.ScrollViewAnimation}
+                    challenge={challenge}
+                />
+                <Animated.ScrollView
+                    onScroll={onScroll}
+                    showsVerticalScrollIndicator={false}
                 >
-                    <Close />
-                </TouchableOpacity>
-                <TouchableOpacity
-                    style={[styles.button, styles.settingsButton]}
-                >
-                    <Settings />
-                </TouchableOpacity>
-                <View style={{ width, height }}>
                     <Challenge
                         challenge={challenge}
                         user={user}
                         plan={plan}
+                        ScrollViewAnimation={this.ScrollViewAnimation}
+                        setContentContainerHeight={setContentContainerHeight}
                     />
-                </View>
+                </Animated.ScrollView>
+                <AnimatedTouchable
+                    style={[styles.button, styles.backButton, { opacity: buttonOpacity }]}
+                    onPress={() => Actions.MyChallengesTab()}
+                >
+                    <Close />
+                </AnimatedTouchable>
+                <AnimatedTouchable
+                    style={[styles.button, styles.settingsButton, { opacity: buttonOpacity }]}
+                >
+                    <Settings />
+                </AnimatedTouchable>
+
+                <AnimatedTouchable
+                    onPress={() => Actions.CreatePost({ challenge })}
+                    style={[styles.addPostButton, { opacity: addPostButtonOpacity }]}
+                >
+                    <Plus fill='#fff' />
+                    <Text style={styles.addPostText}>New Post</Text>
+                </AnimatedTouchable>
             </View>
         );
     }
@@ -77,7 +120,7 @@ const styles = EStyleSheet.create({
     button: {
         position: 'absolute',
         backgroundColor: 'rgba(63, 67, 79, 0.4)',
-        zIndex: 100,
+        zIndex: 2,
         height: 48,
         width: 48,
         borderRadius: 24,
@@ -91,5 +134,24 @@ const styles = EStyleSheet.create({
     settingsButton: {
         top: 25,
         right: 19
+    },
+    addPostButton: {
+        width: 138,
+        height: 56,
+        borderRadius: 28,
+        position: 'absolute',
+        bottom: 36,
+        right: 18,
+        backgroundColor: '#1AA0FF',
+        //box-shadow: 0px 20px 20px rgba(26, 160, 255, 0.25);
+        flexDirection: 'row',
+        paddingHorizontal: 22,
+        justifyContent: 'space-between',
+        alignItems: 'center'
+    },
+    addPostText: {
+        fontFamily: 'Poppins-Medium',
+        fontSize: 16,
+        color: '#FFFFFF'
     }
 });
